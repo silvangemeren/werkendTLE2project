4754d00daad3eecab37fc7aab519fade48ea035c
Yosei10200 <yosei.saito@gmail.com>
2024-12-03 15:35:50 +0100

    vacatures toegevoegd

--- resources/views/makeVacature/index.blade.php
+++ resources/views/makeVacature/index.blade.php
@@ -1,13 +1,4 @@
-<!doctype html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport"
-          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Document</title>
-</head>
-<body>
+<x-layout>
     <h1>Vacature Creeeren</h1>
     <form>
 
@@ -49,5 +40,4 @@
         </div>
 
     </form>
-</body>
-</html>
+</x-layout>
