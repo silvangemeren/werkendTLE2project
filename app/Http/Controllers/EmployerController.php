9aa021ceb37313fd638c0f2ce7b7054054cba728
Jake-HBH <jakehersbach2005@gmail.com>
2024-12-04 14:45:28 +0100

    employee/employer eerste pagina keuze, routes

---
+++ app/Http/Controllers/EmployerController.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class EmployerController extends Controller
+{
+    public function index()
+    {
+        return view('employer'); // Replace with your actual view name
+    }
+}
+
--- routes/web.php
+++ routes/web.php
@@ -1,11 +1,19 @@
 <?php
 
+use App\Http\Controllers\EmployeeController;
+use App\Http\Controllers\EmployerController;
 use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
-    return view('welcome');
+    return view('employee-employer');
 });
+
+
+// Employee and Employer pages routes
+Route::get('/employee', [EmployeeController::class, 'index'])->name('employee-page');
+Route::get('/employer', [EmployerController::class, 'index'])->name('employer-page');
+
 
 Route::get('/dashboard', function () {
     return view('dashboard');
