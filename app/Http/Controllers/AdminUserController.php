18a773bbc9356d4f48518e9dbc1fda4c2932f120
Jake-HBH <jakehersbach2005@gmail.com>
2024-12-10 15:45:23 +0100

    Admin dashboard (zonder css) met user profielen beheren, editen en verwijderen werkend. Alleen nog de resterende kolommen toevoegen

---
+++ app/Http/Controllers/AdminUserController.php
@@ -0,0 +1,60 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\User;
+use Illuminate\Http\Request;
+
+class AdminUserController extends Controller
+{
+    public function index()
+    {
+        // Fetch all users from the database
+        $users = User::all();
+
+        return view('admin.users.index', compact('users'));
+    }
+
+
+    public function edit(User $user)
+    {
+// edit form laten zien van een user
+        return view('admin.users.edit', compact('user')); //zet variabele user om in een array en zorgt dat deze gegevens beschikbaar is in de view
+    }
+
+    public function update(Request $request, User $user)
+    {
+// valideren en update van user details
+        $request->validate([ // controleert of de waarden aan bepaalde regels voldoen
+            'name' => 'required|string|max:255',
+            'email' => 'required|email|max:255',
+            ]);
+
+        $user->update($request->only('name', 'email')); // update van user details //
+
+        return redirect()->route('admin.users.index');
+    }
+
+    public function destroy(User $user)
+    {
+        $user->delete(); // verwijderen user
+        return redirect()->route('admin.users.index'); // redirect naar de user list na verwijdering
+    }
+}
+
+//    public function activate(User $user)
+//    {
+//        $user->status = 1;
+//        $user->save();
+//
+//        return redirect()->route('admin.users.index');
+//    }
+//
+//    public function deactivate(User $user)
+//    {
+//        $user->status = 0;
+//        $user->save();
+//
+//        return redirect()->route('admin.users.index');
+//    }
+//}
