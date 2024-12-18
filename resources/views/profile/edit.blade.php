41d95d7d5b66132e23baf41b4a600be28f362341
Yosei10200 <yosei.saito@gmail.com>
2024-12-18 13:21:16 +0100

    Delete account wordt niet in admin getoond

--- resources/views/profile/edit.blade.php
+++ resources/views/profile/edit.blade.php
@@ -14,11 +14,12 @@
                 </div>
             </div>
 
-            <div class="p-4 sm:p-8 bg-white dark:bg-gray-800 shadow sm:rounded-lg">
-                <div class="max-w-xl">
-                    @include('profile.partials.delete-user-form')
+            @if(auth()->user()->role != 'admin')
+                <div class="p-4 sm:p-8 bg-white dark:bg-gray-800 shadow sm:rounded-lg">
+                    <div class="max-w-xl">
+                        @include('profile.partials.delete-user-form')
+                    </div>
                 </div>
-            </div>
-        </div>
+            @endif
     </div>
 </x-app-layout>
