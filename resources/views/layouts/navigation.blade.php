e1fa9f243cf6db7e9ecbea884a547ed05a078b46
yaelen <yaelensardjo@gmail.com>
2024-12-18 10:23:45 +0100

    nav icons, mobile nav fixes, most log out buttons fixed :)

--- resources/views/layouts/navigation.blade.php
+++ resources/views/layouts/navigation.blade.php
@@ -1,10 +1,11 @@
-<nav x-data="{ open: false }" class="bg-[#2E342A] border-b border-gray-100 dark:border-gray-700 sm:relative sm:top-0 sm:h-20">
+<nav x-data="{ open: false }"
+     class="bg-[#2E342A] border-b border-gray-100 dark:border-gray-700 sm:relative sm:top-0 sm:h-20">
     <!-- Desktop Navigation -->
     <div class="hidden sm:flex max-w-7xl mx-auto px-4 sm:px-6 lg:px-8 justify-between items-center h-20">
         <!-- Left: Logo -->
         <div class="flex items-center">
             <a href="{{ route('dashboard') }}">
-                <x-application-logo class="block h-12 w-auto fill-current text-[#FAEC02]" />
+                <x-application-logo class="block h-12 w-auto fill-current text-[#FAEC02]"/>
             </a>
         </div>
 
@@ -12,32 +13,29 @@
         <div class="flex space-x-8 items-center justify-center">
             @auth
                 @if(auth()->user()->role === 'werkgever' || 'werknemer')
-            <x-nav-link :href="route('dashboard')" :active="request()->routeIs('dashboard')" class="text-[#FAEC02] text-lg font-extrabold">
-                {{ __('Home') }}
-            </x-nav-link>
+                    <x-nav-link :href="route('dashboard')" :active="request()->routeIs('dashboard')"
+                                class="text-[#FAEC02] text-lg font-extrabold">
+                        {{ __('Home') }}
+                    </x-nav-link>
                 @endif
                 @if(auth()->user()->role === 'werkgever')
-                    <x-nav-link :href="route('vacancies.employer')" :active="request()->routeIs('vacancies.employer')" class="text-[#FAEC02] text-lg font-extrabold">
+                    <x-nav-link :href="route('vacancies.employer')" :active="request()->routeIs('vacancies.employer')"
+                                class="text-[#FAEC02] text-lg font-extrabold">
                         {{ __('Vacatures') }}
                     </x-nav-link>
                 @elseif(auth()->user()->role === 'werknemer')
-                    <x-nav-link :href="route('vacancies.employee')" :active="request()->routeIs('vacancies.employee')" class="text-[#FAEC02] text-lg font-extrabold">
+                    <x-nav-link :href="route('vacancies.employee')" :active="request()->routeIs('vacancies.employee')"
+                                class="text-[#FAEC02] text-lg font-extrabold">
                         {{ __('Vacatures') }}
                     </x-nav-link>
                 @endif
-
-                <!-- Logout Button -->
-                <form method="POST" action="{{ route('logout') }}" class="inline">
-                    @csrf
-                    <button type="button" id="logoutButton" class="text-[#FAEC02] text-lg font-extrabold hover:text-red-600">
-                        {{ __('Logout') }}
-                    </button>
-                </form>
             @else
-                <x-nav-link :href="route('home')" :active="request()->routeIs('home')" class="text-[#FAEC02] text-lg font-extrabold">
+                <x-nav-link :href="route('home')" :active="request()->routeIs('home')"
+                            class="text-[#FAEC02] text-lg font-extrabold">
                     {{ __('Home') }}
                 </x-nav-link>
-                <x-nav-link :href="route('vacancies.guest')" :active="request()->routeIs('vacancies.guest')" class="text-[#FAEC02] text-lg font-extrabold">
+                <x-nav-link :href="route('vacancies.guest')" :active="request()->routeIs('vacancies.guest')"
+                            class="text-[#FAEC02] text-lg font-extrabold">
                     {{ __('Vacatures') }}
                 </x-nav-link>
             @endauth
@@ -47,10 +45,12 @@
                 <img src="{{ asset('images/logo-oh.png') }}" alt="Open Hiring Logo" class="max-h-16 h-auto">
             </div>
 
-            <x-nav-link :href="route('home')" :active="request()->routeIs('home')" class="text-[#FAEC02] text-lg font-extrabold">
+            <x-nav-link :href="route('home')" :active="request()->routeIs('home')"
+                        class="text-[#FAEC02] text-lg font-extrabold">
                 {{ __('Inbox') }}
             </x-nav-link>
-            <x-nav-link :href="route('profile.edit')" :active="request()->routeIs('profile.edit')" class="text-[#FAEC02] text-lg font-extrabold">
+            <x-nav-link :href="route('profile.edit')" :active="request()->routeIs('profile.edit')"
+                        class="text-[#FAEC02] text-lg font-extrabold">
                 {{ __('Profile') }}
             </x-nav-link>
         </div>
@@ -60,7 +60,8 @@
             <div>
                 <form method="POST" action="{{ route('logout') }}" class="inline">
                     @csrf
-                    <button type="button" id="logoutButton" class="text-[#FAEC02] text-lg font-extrabold hover:text-red-600">
+                    <button type="button" id="logoutButton"
+                            class="text-[#FAEC02] text-lg font-extrabold hover:text-red-600">
                         {{ __('Logout') }}
                     </button>
                 </form>
@@ -73,7 +74,7 @@
         <div class="flex justify-around items-center h-16">
             <!-- Home Button -->
             <a href="{{ route('dashboard') }}" class="flex flex-col items-center text-[#FAEC02] text-xl font-extrabold">
-                <img src="{{ asset('images/Icons/Dashboard_nav.svg') }}" alt="Logout Icon" class="h-6 w-6">
+                <img src="{{ asset('images/Icons/Dashboard_nav.svg') }}" alt="Dashboard Icon" class="h-6 w-6">
                 <span class="text-xs">{{ __('Dashboard') }}</span>
             </a>
 
@@ -81,13 +82,12 @@
             @auth
                 @if(auth()->user()->role === 'werkgever')
                     <a href="{{ route('vacancies.employer') }}" class="flex flex-col items-center text-[#FAEC02] text-xl font-extrabold">
-                        <img src="{{ asset('images/Icons/Vacancy_nav.svg') }}" alt="Logout Icon" class="h-6 w-6">
+                        <img src="{{ asset('images/Icons/Vacancy_nav.svg') }}" alt="Vacancies Icon" class="h-6 w-6">
                         <span class="text-xs">{{ __('Vacatures') }}</span>
                     </a>
-
                 @elseif(auth()->user()->role === 'werknemer')
                     <a href="{{ route('vacancies.employee') }}" class="flex flex-col items-center text-[#FAEC02] text-xl font-extrabold">
-                        <img src="{{ asset('images/Icons/Vacancy_nav.svg') }}" alt="Logout Icon" class="h-6 w-6">
+                        <img src="{{ asset('images/Icons/Vacancy_nav.svg') }}" alt="Vacancies Icon" class="h-6 w-6">
                         <span class="text-xs">{{ __('Vacatures') }}</span>
                     </a>
                 @endif
@@ -95,16 +95,16 @@
                 <!-- Logout Button -->
                 <form method="POST" action="{{ route('logout') }}" class="inline">
                     @csrf
-                    <button type="button" id="logoutButton" class="flex flex-col items-center text-[#FAEC02] text-xl font-extrabold"> <img src="{{ asset('images/Icons/Logout_nav.svg') }}" alt="Logout Icon" class="h-6 w-6">
+                    <button type="submit" id="logoutButton" class="flex flex-col items-center text-[#FAEC02] text-xl font-extrabold">
+                        <img src="{{ asset('images/Icons/Logout_nav.svg') }}" alt="Logout Icon" class="h-6 w-6">
                         <span class="text-xs">{{ __('Logout') }}</span>
                     </button>
                 </form>
             @else
                 <a href="{{ route('vacancy.index') }}" class="flex flex-col items-center text-[#FAEC02] text-xl font-extrabold">
-                    <img src="{{ asset('images/Icons/Vacancy_nav.svg') }}" alt="Logout Icon" class="h-6 w-6">
+                    <img src="{{ asset('images/Icons/Vacancy_nav.svg') }}" alt="Vacancies Icon" class="h-6 w-6">
                     <span class="text-xs">{{ __('Vacatures') }}</span>
                 </a>
-
             @endauth
 
             @guest
@@ -115,6 +115,7 @@
             @endguest
         </div>
     </div>
+
 </nav>
 
 <!-- Modal for Logout Confirmation -->
